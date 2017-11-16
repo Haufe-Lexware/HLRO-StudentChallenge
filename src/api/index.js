2720a9afb8e184c64e5468bbec8453478330c9c1
Nacev <NacevB@haufe.com>
2017-11-16 10:13:10 +0200

    API server first draft + strand alone running batch file + modules

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,32 @@
 import express from 'express';
 import { MongoClient, ObjectID } from 'mongodb';
 import assert from 'assert';
-
+import bodyParser from 'body-parser';
 
 
 const router = express.Router();
 
-router.get('/tickets', (req, resp) =>{
-  let tickets = {};
+router.get('/tickets', (req, res) =>{
+  let tickets = [{a: 'g'}, {a:'u'}];
   console.log('In /tickets')
+  res.send(tickets);
+  return tickets;
 });
 
-router.get('/tickets/{ticket_id}', (req, resp) =>{
-  let tickets = {};
-  console.log('In /tickets/tickets_id')
+router.get('/tickets/:ticket_id', (req, res) =>{
+   const ticketId = req.params.ticket_id;
+  let ticket = {a:'b'};
+  console.log('In /tickets/',ticketId);
+  res.send(ticket);
+});
+
+const server = express();
+server.use('/api', router);
+server.use(express.static('public'));
+server.use(bodyParser.json());
+
+server.listen(4000, '0.0.0.0', () => {
+  console.info('API server runni', 4000);
 });
 
 export default router;
